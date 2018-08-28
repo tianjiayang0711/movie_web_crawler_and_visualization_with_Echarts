6568479a2cebeff7c49266488fd22f72e8377a4f
Jiayang Tian <tjyjiayutong@163.com>
2018-08-28 21:46:10 +0800

    Add files via upload

---
+++ movie crawler and visualization/crawler/insert_to_DB.py
@@ -0,0 +1,45 @@
+import MySQLdb
+import MySQLdb.cursors
+
+inputFile = 'clean_data'
+fr = open(inputFile, 'r')
+
+firstLine = True
+
+db = MySQLdb.connect(host='localhost', user='root', passwd='', db='douban', port=3306, charset='utf8', cursorclass = MySQLdb.cursors.DictCursor)
+db.autocommit(True)
+cursor = db.cursor()
+
+count = 0
+
+for line in fr:
+	if firstLine:
+		firstLine = False
+		continue
+
+	line = line.split('^')
+
+	movieId= line[0]
+	title = line[1]
+	url = line[2]
+	cover = line[3]
+	rate = line[4]
+	director = line[5]
+	composer = line[6]
+	actor = line[7]
+	category = line[8]
+	district = line[9]
+	language = line[10]
+	showtime = line[11]
+	length = line[12]
+	othername = line[13]
+	description = line[14]
+
+	cursor.execute('insert into movie(movieId,title,url,cover,rate,director,composer,actor,category,district,language,showtime,length,othername,description) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',[movieId,title,url,cover,rate,director,composer,actor,category,district,language,showtime,length,othername,description])
+
+	count = count + 1
+	print count, title
+
+fr.close()
+db.close()
+cursor.close()
